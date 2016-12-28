e6327f97b2227e7681dc6922fd9f03ed445641c2
justinhessdev <justinhessdev@gmail.com>
2016-12-27 21:31:44 -0800

    added sessions, user links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,12 @@ class UsersController < ApplicationController
   end
 
   def create
-  end
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end  end
 
   def edit
   end
@@ -20,5 +25,9 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
