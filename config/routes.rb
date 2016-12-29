987b043e533f71cf2a369917fad617ca9c781571
justinhessdev <justinhessdev@gmail.com>
2016-12-28 16:06:40 -0800

    added Post with image logic

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   root 'users#index'
   resources :users
-
+  resources :posts
   delete '/logout' => 'sessions#destroy', as: :logout
   resources :sessions, only: [:new, :create]
-  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161221185847) do
+ActiveRecord::Schema.define(version: 20161228210819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "content"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
